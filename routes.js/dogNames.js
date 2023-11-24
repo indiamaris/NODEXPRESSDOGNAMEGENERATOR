d6e89f9566436db46cf1b34e6cba9e203f699cb8
indiamaris <indiamaris@gmail.com>
2023-11-24 05:50:29 +0100

    creating Joi validation and custom messages

--- routes.js/dogNames.js
+++ routes.js/dogNames.js
@@ -27,11 +27,39 @@ const dogNameSchema = new moongose.Schema({
 
 const validateDogName = (dogName) => {
 	const schemaDogName = Joi.object({
-		dogName: Joi.string().alphanum().min(3).max(10).required(),
+		dogName: Joi.string()
+			.pattern(/^[a-zA-Z]+$/)
+			.min(3)
+			.max(15)
+			.required()
+			.messages({
+				'string.required': 'We need a name for a dog name .',
+				'string.pattern.base':
+					'Sorry,we require only English alphabet  for a name.',
+				'string.min':
+					'Sorry, we require a minimum 3 character  for a name.',
+				'string.max':
+					'Sorry, we require a maximum 15 character for a name.',
+			}),
 	});
 	return schemaDogName.validate(dogName);
 };
 
+const validateId = (id) => {
+	const schemaDogID = Joi.object({
+		id: Joi.string().min(24).max(25).required(),
+		// .messages({
+		// 	'string.required': 'We need a id here .',
+		// 	'string.hex':
+		// 		'Sorry, we require a hexadecimal for a id.',
+		// 	'string.min':
+		// 		'Sorry, we require a minimum 24 character  for a id.',
+		// 	'string.max':
+		// 		'Sorry, we require a maximum 25 character for a id.',
+		// }),
+	});
+	return schemaDogID.validate(id);
+};
 const data = () => DogName.find().sort('dogName');
 const dataByName = (dogName) => DogName.find({ dogName: dogName });
 const dataById = (id) => DogName.findById(id);
@@ -79,18 +107,79 @@ router.get('/name/:name', async (req, res) => {
 // ___________*****_POST__ROUTES_*****___________________//
 
 router.post('/allNames', async (req, res) => {
-	const dogName = new DogName({ dogName: req.body.name });
 	try {
+		const { error } = validateDogName(req.body);
+		if (error) return res.send(error.details[0].message);
+		const dogName = new DogName({ dogName: req.body.dogName });
 		await dogName.save();
 
-	} catch (ex) {
-		console.log('the error :' + ex.message);
+		res.send(dogName);
+	} catch (error) {
+		console.log('the error :' + error.message);
+		res.send(error);
 	}
-			res.send(dogName);
 });
 
+// ___________*****_PATCH__ROUTE_*****___________________//
+router.patch('/id/:id', async (req, res) => {
+	// primeiro validar se o id eh valido
+	try {
+		const { error } = validateId(req.params);
+		if (error) {
+			return res.send(error.details[0].message);
+		} else {
+			const { error } = validateDogName(req.body);
+			if (error) return res.send(error.details[0].message);
+		}
 
-		// validateDogName(dogName) && res.status(404).send('Plase, dont panic!');
+		let dogName = req.body.dogName;
+		const dog = await DogName.findByIdAndUpdate(
+			req.params.id,
+			{
+				dogName: dogName,
+			},
+			{ returnDocument: 'after' }
+		);
+		const dogUpdated = dog;
+
+		res.send(dogUpdated);
+	} catch (error) {
+		console.log('the error :' + error.message);
+		res.send(error);
+	}
+});
+
+// const paramExist =async(param)=> await DogName.exists({ `${param}`: `req.params.${param}` });
+
+// ___________*****_DELETE__ROUTES_*****___________________//
+router.delete('/id/:id', async (req, res) => {
+	try {
+		const { error } = validateId(req.params);
+		if (error) {
+			return res.send(error.details[0].message);
+		}
+
+
+		const idExist = await DogName.exists({ _id: req.params.id });
+		!idExist &&
+			res
+				.status(400)
+				.send(
+					` Sorry, there is no dog's name with the id ${req.params.id}.  `
+				);
+
+		const dog = await DogName.findByIdAndDelete(req.params.id, {
+			returnDocument: 'after',
+		});
+		res.send(
+			` The ${dog.dogName}'s name was deleted from database , but certanly ${dog.dogName} was a good one. ${dog.dogName}'s id used to be:${dog.id} `
+		);
+	} catch (err) {
+		if (err.reason === '{}')
+			res.send('The id do not exist in the database');
+	}
+});
+
 // ___________*****_PUT__ROUTES_*****___________________//
 // We cannot change the DB ID
 // router.put('/id/:id', async (req, res) => {
@@ -107,32 +196,6 @@ router.post('/allNames', async (req, res) => {
 
 // 	res.send(dogUpdated);
 // });
-// ___________*****_PATCH__ROUTE_*****___________________//
-router.patch('/id/:id', async (req, res) => {
-	let dogName = req.body.dogName;
-	const dog = await DogName.findByIdAndUpdate(
-		req.params.id,
-		{
-			dogName: dogName,
-		},
-		{ returnDocument: 'after' }
-	);
-	const dogUpdated = dog;
-
-	res.send(dogUpdated);
-});
-
-// ___________*****_DELETE__ROUTES_*****___________________//
-router.delete('/id/:id', async (req, res) => {
-	const dog = await DogName.findByIdAndDelete(req.params.id, {
-		returnDocument: 'after',
-	});
-
-	res.send(
-		` The ${dog.dogName}'s name was deleted from database , but certanly ${dog.dogName} was a good one.${dog.dogName}'s id used to be:${dog.id} `
-	);
-});
 module.exports = router;
-
 
 
