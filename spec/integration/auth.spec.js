419efd73dbe431b0ead1dcf789ff3c810310e986
indiamaris <indiamaris@gmail.com>
2024-01-03 14:24:23 +0100

    tudo funcionando com -- leak test

--- spec/integration/auth.spec.js
+++ spec/integration/auth.spec.js
@@ -1,28 +1,51 @@
 /** @format */
-
+const { DogName } = require('../../models/dogName');
 const { User } = require('../../models/user');
 const request = require('supertest');
+let server;
 describe('auth-middleware', () => {
-	beforeEach(async () => {
+	const closeServer = (server) => {
+		jest.useRealTimers();
+		return new Promise((resolve) => {
+			server.close((err) => {
+				resolve();
+			});
+		});
+	};
+	beforeEach(() => {
 		server = require('../../index');
+		token = new User().generateAuthToken();
 	});
 
 	afterEach(async () => {
-		server.close();
+		jest.setTimeout(100000);
+		await closeServer(server);
+		await DogName.deleteMany({});
 	});
+	let token;
 
 	const exec = () => {
-		let token = new User().generateAuthToken();
-		return request(server).post('/api/allNames').set('x-auth-token', token);
+		return request(server)
+			.post('/api/allNames')
+			.set('x-auth-token', token)
+			.send({ dogName: 'HENRICO' });
 	};
 
-	it('should return 403 if no token is provided', async () => {
+	it('should return 401 if no token is provided', async () => {
+		token = '';
 		const res = await exec();
-		expect(res.status).toBe(403);
+		expect(res.status).toBe(401);
 	});
-	// it('should return 200 if token is provided', async () => {
-	// 	const res = await exec();
-	// 	expect(res.status).toBe(200);
-	// });
+
+	it('should return 400 if no valid token is provided', async () => {
+		token = null;
+		const res = await exec();
+		expect(res.status).toBe(400);
+	}); 
+	it('should return 200 if token is provided', async () => {
+		const res = await exec();
+		expect(res.status).toBe(200);
+	});
 });
+
 
--- spec/unit/models/user.spec.js
+++ spec/unit/models/user.spec.js
@@ -3,12 +3,16 @@ const config = require('config');
 const jwt = require('jsonwebtoken');
 const { User } = require('../../../models/user');
 const mongoose = require('mongoose');
-describe.skip('Generate Auth Token', () => {
+describe('Generate Auth Token', () => {
 	it('return a valid JWT token', () => {
-		const payload = { _id: new mongoose.Types.ObjectId().toHexString(), isAdmin: true };
+		const payload = {
+			_id: new mongoose.Types.ObjectId().toHexString(),
+			isAdmin: true,
+		};
 		const user = new User(payload);
 		const token = user.generateAuthToken();
 		const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
 		expect(decoded).toMatchObject(payload);
 	});
 });
+
